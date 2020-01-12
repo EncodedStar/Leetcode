20a0511f02b0f40fbbedc33a7747d6744e0d4b68
hx <478672241@qq.com>
2020-01-12 13:42:33 +0800

    updata

--- 0112/283.cpp
+++ 0112/283.cpp
@@ -1,10 +1,7 @@
 class Solution {
 	public:
 		void moveZeroes(vector<int>& nums) {
-			int fast = 0;
 			int slow = 0;
-			if (nums.size() == 1)
-				return;
 			for (int fast = 0 ; fast < nums.size() ; fast++) {
 				if (nums[fast] != 0 ) {
 					if (fast == slow){
