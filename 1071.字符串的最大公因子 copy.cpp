f1f1bf52168b56bde47af96645213027a4465fed
Win EncodeStar <478672241@qq.com>
2020-03-12 10:37:08 +0800

    每日一题：20200312

---
+++ 1071.字符串的最大公因子 copy.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1071 lang=cpp
+ *
+ * [1071] 字符串的最大公因子
+ */
+
+// @lc code=start
+static const auto _ = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        return str1.substr(0, __gcd((int)str1.length(), (int)str2.length())); // __gcd() 为c++自带的求最大公约数的函数
+    }
+};
+// @lc code=end
+
