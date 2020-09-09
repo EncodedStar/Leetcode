ad6db6be8061b3d289611bdb8e69fc0640ab1c72
Win EncodeStar <478672241@qq.com>
2020-09-09 10:02:30 +0800

    中位数

---
+++ 题库练习/347. 前 K 个高频元素.cpp
@@ -0,0 +1,58 @@
+/*
+347. 前 K 个高频元素
+给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+
+
+示例 1:
+
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+示例 2:
+
+输入: nums = [1], k = 1
+输出: [1]
+ 
+
+提示：
+
+你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+你可以按任意顺序返回答案。
+
+*/
+
+class Solution {
+public:
+    static bool cmp(pair<int, int>& m, pair<int, int>& n) {
+            return m.second > n.second;
+        }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> m_map;
+        vector<int> result;
+        for (auto& v : nums) {
+            m_map[v]++;
+        }
+        // pair 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(&cmp)> q(cmp);
+        for (auto& [num, count] : occurrences) {
+            if (q.size() == k) {
+                if (q.top().second < count) {
+                    q.pop();
+                    q.emplace(num, count);
+                }
+            } else {
+                q.emplace(num, count);
+            }
+        }
+        vector<int> ret;
+        while (!q.empty()) {
+            ret.emplace_back(q.top().first);
+            q.pop();
+        }
+        return ret;
+
+
+    }
+};
