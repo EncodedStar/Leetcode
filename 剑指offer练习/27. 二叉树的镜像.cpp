a3391fabcea97e7911f5bbe63e115b3ed29d6e17
Win EncodeStar <478672241@qq.com>
2020-09-11 18:40:07 +0800

    更新

---
+++ 剑指offer练习/27. 二叉树的镜像.cpp
@@ -0,0 +1,69 @@
+/*
+剑指 Offer 27. 二叉树的镜像
+请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+
+例如输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+镜像输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+示例 1：
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+ 
+限制：
+0 <= 节点个数 <= 1000
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        swap(root->left, root->right);
+        mirrorTree(root->left);
+        mirrorTree(root->right);
+        return root;
+    }
+};
+
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (node == NULL) {
+                continue;
+            }
+            swap(node->left, node->right);
+            q.push(node->left);
+            q.push(node->right);
+        }
+        return root;
+    }
+};
+
+
