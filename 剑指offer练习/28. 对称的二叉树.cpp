ae0d33aaec108c7893fe33a13551d878d6b32850
EncodedStar <478672241@qq.com>
2020-09-12 14:11:45 +0800

    对称树

---
+++ 剑指offer练习/28. 对称的二叉树.cpp
@@ -0,0 +1,56 @@
+/*
+剑指 Offer 28. 对称的二叉树
+请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+ 
+限制：
+0 <= 节点个数 <= 1000
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return root == NULL ? true : helper(root->left,root->right);
+    }
+    bool helper(TreeNode* L, TreeNode* R)
+    {
+        if(L == NULL && R == NULL) return true;
+        if(L == NULL || R == NULL || L->val != R->val) return false;
+        return helper(L->left, R->right) && helper(L->right, R->left);
+    }
+};
+
